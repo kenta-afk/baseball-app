421fd3904d91d3470202274c9efc3ad2ba199a8e
kenta-afk <kentarou9.60@icloud.com>
2024-10-16 15:23:51 +0900

    vuetifyでの書き直し

--- resources/js/Pages/Auth/VerifyEmail.vue
+++ resources/js/Pages/Auth/VerifyEmail.vue
@@ -1,7 +1,5 @@
 <script setup>
 import { computed } from 'vue';
-import GuestLayout from '@/Layouts/GuestLayout.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
 import { Head, Link, useForm } from '@inertiajs/vue3';
 
 const props = defineProps({
@@ -16,13 +14,11 @@ const submit = () => {
     form.post(route('verification.send'));
 };
 
-const verificationLinkSent = computed(
-    () => props.status === 'verification-link-sent',
-);
+const verificationLinkSent = computed(() => props.status === 'verification-link-sent');
 </script>
 
 <template>
-    <GuestLayout>
+    <v-container>
         <Head title="Email Verification" />
 
         <div class="mb-4 text-sm text-gray-600">
@@ -31,31 +27,25 @@ const verificationLinkSent = computed(
             didn't receive the email, we will gladly send you another.
         </div>
 
-        <div
-            class="mb-4 text-sm font-medium text-green-600"
-            v-if="verificationLinkSent"
-        >
-            A new verification link has been sent to the email address you
-            provided during registration.
-        </div>
+        <v-alert v-if="verificationLinkSent" type="success" class="mb-4">
+            A new verification link has been sent to the email address you provided during registration.
+        </v-alert>
 
-        <form @submit.prevent="submit">
-            <div class="mt-4 flex items-center justify-between">
-                <PrimaryButton
-                    :class="{ 'opacity-25': form.processing }"
-                    :disabled="form.processing"
-                >
+        <v-form @submit.prevent="submit">
+            <div class="d-flex justify-between align-center">
+                <v-btn :loading="form.processing" color="primary" type="submit">
                     Resend Verification Email
-                </PrimaryButton>
+                </v-btn>
 
                 <Link
                     :href="route('logout')"
                     method="post"
                     as="button"
-                    class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                    >Log Out</Link
+                    class="text-sm text-gray-600 underline hover:text-gray-900"
                 >
+                    Log Out
+                </Link>
             </div>
-        </form>
-    </GuestLayout>
+        </v-form>
+    </v-container>
 </template>
